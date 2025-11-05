7b256a91979827f0cea6e0aed9bb6c25f25b9a58
Suraj <suraj@example.com>
2025-11-05 12:50:35 +0530

    add task functionality

---
+++ ToDo/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponse
+from ToDo.models import Task
+
+def add_task(request):
+    task=request.POST.get['task']
+    Task.objects.create(task=task)
+    return redirect('home')
+
+def mark_as_done(request, pk):
+    task = get_object_or_404(Task, pk=pk)
+    task.is_completed = True
+    task.save()
+    return redirect('home')
--- todo_main/views.py
+++ todo_main/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from ToDo.models import Task
+from django.shortcuts import get_object_or_404 
 
 def home(request):
-    return render(request, 'home.html')
+    tasks= Task.objects.filter(is_completed=False).order_by('-created_at')
+    context={
+        'tasks':tasks,
+        }
+
+    return render(request, 'home.html', context)
