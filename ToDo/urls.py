327c3c3352bd52b477a0547f2a5d32920f814f28
Suraj <suraj@example.com>
2025-11-06 15:00:02 +0530

    todo app final

--- ToDo/urls.py
+++ ToDo/urls.py
@@ -2,5 +2,14 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('addTask/', views.add_task, name='add_task'),
-    path('done/<int:pk>/', views.mark_as_done, name='mark_as_done')]
+    # add task
+    path('add_Task/', views.add_Task, name='add_Task'),
+    # mark as done
+    path('mark_as_done/<int:pk>/', views.mark_as_done, name='mark_as_done'),
+    # mark as undone
+    path('mark_as_undone/<int:pk>/', views.mark_as_undone, name='mark_as_undone'),
+    # edit features
+    path('edit_task/<int:pk>/', views.edit_task, name='edit_task'),
+    # delete task
+    path('delete_task/<int:pk>/', views.delete_task, name='delete_task'),
+]
--- todo_main/views.py
+++ todo_main/views.py
@@ -4,8 +4,10 @@ from django.shortcuts import get_object_or_404
 
 def home(request):
     tasks= Task.objects.filter(is_completed=False).order_by('-created_at')
+    completed_tasks= Task.objects.filter(is_completed=True)
     context={
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
         }
 
     return render(request, 'home.html', context)
